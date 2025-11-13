441b40e7acc9484a0e27254ffb6ff75368e78e65
Major <yoyov5123@163.com>
2025-11-14 06:52:06 +0800

    update to use remote driver

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,31 +21,49 @@ public class DriverFactory {
 
     public static WebDriver createDriver() {
 
-        String browser = ConfigReader.getProperty("browser");
-        if (browser.equalsIgnoreCase("chrome")) {
+        String onHub = System.getProperty("onHub", ConfigReader.getProperty("onHub"));
+        String browser = System.getProperty("browser", ConfigReader.getProperty("browser"));
+        String hubUrl = System.getProperty("hubUrl", ConfigReader.getProperty("hubUrl"));
 
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
+
+        ChromeOptions options = new ChromeOptions();
 //            options.addArguments("--no-sandbox");
-            //set the mode here
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--remote-allow-origins=*");
+        //set the mode here
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("download.default_directory", downloadDir);
+        prefs.put("download.prompt_for_download", false);
+        options.setExperimentalOption("prefs", prefs);
 
 
-            Map<String, Object> prefs = new HashMap<>();
-            prefs.put("download.default_directory", downloadDir);
-            prefs.put("download.prompt_for_download", false);
-            options.setExperimentalOption("prefs", prefs);
+        if(onHub.equals("true")){
+            try{
 
-            return new ChromeDriver(options);
+                return new RemoteWebDriver(new URL(hubUrl), options);
 
-        } else   {
-            WebDriverManager.firefoxdriver().setup();
-            return new FirefoxDriver();
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Invalid hub URL: " + hubUrl, e);
+            }
+
+
+        } else {
+            if (browser.equalsIgnoreCase("chrome")) {
+
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver(options);
+
+            } else   {
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            }
         }
+
+
 
     }
 
